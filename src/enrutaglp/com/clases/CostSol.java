494f26ce6b0939d26772d76fcf7757a8f02bcfcb
StevRamos <stevhr@gmail.com>
2021-09-15 15:05:55 -0500

    xd

---
+++ src/enrutaglp/com/clases/CostSol.java
@@ -0,0 +1,18 @@
+package enrutaglp.com.clases;
+
+public class CostSol {
+	private double penalizedCost;
+	private int nbRoutes;
+	private double distance;	
+	private double capacityExcess;
+	private double durationExcess;	
+
+	public CostSol() {
+		// TODO Auto-generated constructor stub
+		this.penalizedCost = 0;
+		this.nbRoutes = 0;
+		this.distance = 0;
+		this.capacityExcess = 0;
+		this.durationExcess = 0;
+	}
+}
