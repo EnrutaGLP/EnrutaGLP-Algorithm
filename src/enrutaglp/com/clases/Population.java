89ce3a1129cb7009f47dd61255ebcd73111aa597
Diego <rodriguez.diego@pucp.edu.pe>
2021-09-15 17:24:40 -0500

    Population and main changed

---
+++ src/enrutaglp/com/clases/Population.java
@@ -0,0 +1,44 @@
+package enrutaglp.com.clases;
+
+import java.util.Vector;
+
+public class Population {
+
+	private Vector<Individual>individuals; 
+	private int size;
+	private int mu; 
+	private int epsilon; 
+	
+	public Population(int mu, int epsilon) {
+		this.mu = mu; 
+		this.epsilon = epsilon;
+	}
+	
+	public void generatePopulation() {
+		for(int i=0; i<mu;i++) {
+			Individual individual = new Individual();
+		}
+		
+	}
+	
+	public void addIndividual(Individual individual) {
+		individuals.add(individual);
+		size++;
+	}
+	
+	public Individual getBinaryTournament() {
+		int place1, place2; 
+		while(true) {
+			place1 = (int)(Math.random()*size);
+			place2 = (int)(Math.random()*size);
+			if(place1!=place2)break; 
+		}
+		
+		Individual ind1 = individuals.get(place1); 
+		Individual ind2 = individuals.get(place2); 
+		//return the one with the highest fitness 
+		return ind1;
+	}
+	
+	
+}
